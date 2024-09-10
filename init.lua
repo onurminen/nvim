efeede4dc48734742c6245f2fcda3510fbcf37f3
Olli Nurminen <ollinurminen9@gmail.com>
2024-09-10 22:26:38 +0300

    progress

---
+++ init.lua
@@ -0,0 +1,25 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Reset cursor type to vertical bar on exit
+vim.cmd([[
+  autocmd VimLeave * set guicursor=a:ver25
+]])
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = false
+
+-- [[ Setting options ]]
+require 'options'
+
+-- [[ Basic Keymaps ]]
+require 'keymaps'
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+require 'lazy-bootstrap'
+
+-- [[ Configure and install plugins ]]
+require 'lazy-plugins'
